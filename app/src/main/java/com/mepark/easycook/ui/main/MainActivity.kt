e8fc9ca05800c09e2bd2ab6393eb67fbe6809ef1
elmumu99 <xhanwfl@gmail.com>
2022-06-30 17:15:44 +0900

    20220630

--- app/src/main/java/com/mepark/easycook/ui/main/MainActivity.kt
+++ app/src/main/java/com/mepark/easycook/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mepark.easycook.ui.main
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -14,19 +15,32 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : CommonActivity<ActivityMainBinding>(R.layout.activity_main) {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //초기화
+        supportFragmentManager.beginTransaction().replace(R.id.linearLayout , MainFragment()).commitAllowingStateLoss()
 
-        binding.tvMain.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.mainFragment, null)
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.action_recipe ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.linearLayout , RecipeFragment()).commitAllowingStateLoss()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_main ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.linearLayout , MainFragment()).commitAllowingStateLoss()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_refrigerator ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.linearLayout , RefrigeratorFragment()).commitAllowingStateLoss()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
         }
-        binding.tvRecipe.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.recipeFragment, null)
-        }
-        binding.tvRefrigerator.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.refrigeratorFragment, null)
-        }
-
-
     }
+
+
 }
---
+++ app/src/main/java/com/mepark/easycook/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.mepark.easycook.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+}
